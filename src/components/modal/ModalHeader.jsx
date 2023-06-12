6d55ad2f56c7d4e97c431d4edf70d6dcc28c1fe9
Marcos Vieira <marcosvieira482@gmail.com>
2023-06-12 19:08:25 -0300

    feat: improved modal size

--- src/components/modal/ModalHeader.jsx
+++ src/components/modal/ModalHeader.jsx
@@ -4,7 +4,7 @@ export default function ModalHeader(props) {
 
     return (
         <div className="flex justify-center ">
-            <iframe className="w-[300px] h-[180px] md:w-[400px] md:h-[250px] lg:w-[640px] lg:h-[360px]" src={props.link} frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+            <iframe className="w-[300px] h-[180px] md:w-[426px] md:h-[240px] lg:w-[560px] lg:h-[320px] xl:w-[640px] xl:h-[360px]" src={props.link} frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
         </div>
     )
 }
