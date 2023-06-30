3d5443d13c9c033375ec6d98cb540b6b25ac1f66
Marcos Vieira <marcosvieira482@gmail.com>
2023-06-30 18:46:42 -0300

    feat: correction of graphic elements in the skills section

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -3,8 +3,8 @@ import React from "react"
 export default function Skill(props) {
 
     return (
-        <div className="cursor-pointer hover:scale-110 flex flex-col mx-2 my-2 items-center space-y-3 py-3 px-2 lg:px-3 bg-darker rounded-br-xl rounded-tl-xl border-b-4 border-blue-500 w-[90px] lg:w-[120px]">
-            <p className="text-md text-blue-100 ml-0 mr-0 lg:text-lg ">{props.text}</p>
+        <div className="cursor-pointer hover:scale-110 flex flex-col mx-2 my-2 items-center space-y-3 py-3 bg-darker rounded-br-xl rounded-tl-xl border-b-4 border-blue-500  h-[130px] w-[90px] lg:h-[160px] lg:w-[125px]">
+            <p className="text-md text-blue-100 ml-0 mr-0 sm:text-sm lg:text-lg ">{props.text}</p>
             <img src={`./images/skills/${props.logo}.svg`} className="h-[61px] lg:h-[71px]" />
           </div>
     )
