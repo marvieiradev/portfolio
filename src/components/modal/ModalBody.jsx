01209f54da8449b9d68465049ae31844adf8caf6
marvieiradev <marcosvieira482@gmail.com>
2025-02-15 12:59:52 -0300

    feat: update styles on Modal and Header

--- src/components/modal/ModalBody.jsx
+++ src/components/modal/ModalBody.jsx
@@ -5,7 +5,7 @@ export default function ModalBody(props) {
     return (
         <div className="flex flex-col justify-center p-4 space-y-2 lg:justify-start">
             <h1 id="md-title" className="titles text-2xl text-center font-bold text-blue-300 lg:text-2xl">{props.titulo}</h1>
-            <p className="text-xs lg:text-base">{props.subtitulo}</p>
+            <p className="text-xs text-justify lg:text-base">{props.subtitulo}</p>
             <p className="text-xs lg:text-base" dangerouslySetInnerHTML={{ __html: props.infos }}></p>
             <p className="text-md font-bold text-blue-500 md:text-lg">{props.data}</p>
         </div>
--- src/components/modal/ModalButton.jsx
+++ src/components/modal/ModalButton.jsx
@@ -1,10 +1,11 @@
 import React from "react"
+import { CircleX } from "lucide-react"
 
 export default function ModalButton(props) {
 
     return (
         <div className="flex justify-end z-31">
-            <button className="no-title text-base lg:text-lg text-white px-2">X</button>
+            <button className="no-title text-base lg:text-lg text-white px-2 mb-2"><CircleX /></button>
         </div>
     )
 }
