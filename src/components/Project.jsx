7701d06d54d02d251355403b5709708f9bbc3637
marvieiradev <marcosvieira482@gmail.com>
2025-03-17 21:20:46 -0300

    feat: add styles to projects components

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,8 +3,8 @@ import React from "react"
 export default function Project(props) {
 
     return (
-        <div className={`${props.classe} flex flex-col cursor-pointer mx-3 my-3 hover:scale-110 w-[300px] h-[200px] lg:w-[300px] shadow-xl rounded-2xl justify-center`} >
-            <div className="flex flex-col bg-darker bg-opacity-90 justify-center items-center space-y-2 rounded-xl w-[300px] h-[200px] lg:w-[300px] hover:opacity-0">
+        <div className={`${props.classe} flex flex-col cursor-pointer mx-3 my-3 hover:scale-110 w-[300px] h-[200px] lg:w-[300px] shadow-xl rounded-br-2xl rounded-tl-2xl justify-center border-blue-500 border-[3px]`} >
+            <div className="flex flex-col bg-darker bg-opacity-95 justify-center items-center space-y-2 rounded-br-xl rounded-tl-xl w-[294px] h-[194px] lg:w-[294px] hover:opacity-0">
                 <p className="text-xl shadow-3xl">{props.titulo}</p>
                 <div className="flex flex-row item-center space-x-2">
                     <img className={`w-[20px] lg:w-[30px] bg-darker rounded-3xl ${props.cls1}`} src={`./images/skills/${props.img1}.svg`} />
@@ -12,7 +12,6 @@ export default function Project(props) {
                     <img className={`w-[20px] lg:w-[30px] bg-darker rounded-3xl ${props.cls3}`} src={`./images/skills/${props.img3}.svg`} />
                     <img className={`w-[20px] lg:w-[30px] bg-darker rounded-3xl ${props.cls4}`} src={`./images/skills/${props.img4}.svg`} />
                     <img className={`w-[20px] lg:w-[30px] bg-darker rounded-3xl ${props.cls5}`} src={`./images/skills/${props.img5}.svg`} />
-
                 </div>
             </div>
         </div>
